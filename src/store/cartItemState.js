247825f597d010218a0298c5723be6b691b5153b
Vaibhav Joshi <joshivaibhavjoc@gmail.com>
2025-10-22 19:22:40 +0530

    Removed cart counter atom

--- src/store/cartItemState.js
+++ src/store/cartItemState.js
@@ -1,10 +1,5 @@
 import { atom } from "recoil";
 
-export const cartCounter = atom({
-    key: "cartCounter",
-    default: 0
-})
-
 export const cartItem = atom({
     key: "cartItems",
     default: []
